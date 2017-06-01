2481f81f6e636008c41ee015947d68d772b05981
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2017-06-02 01:30:11 +0200

    Join main files

--- Makefile
+++ Makefile
@@ -1,17 +1,11 @@
 all:# test1 test2
 	g++ main.cpp -o main.o -g -c -Wall -Wshadow -O2
 	nasm -f elf64 test1.asm -o test1.o -g
-	g++ main.o test1.o -o test -g -O2
-	g++ main2.cpp -o main2.o -g -c -Wall -Wshadow -O2
 	nasm -f elf64 test2.asm -o test2.o -g
-	g++ main2.o test2.o -o test2 -g -O2
-#test2:
-#test1:
+	g++ main.o test1.o test2.o -o test -g -O2
 
 clean:
 	rm test1.o
 	rm main.o
 	rm test
 	rm test2.o
-	rm main2.o
-	rm test2
--- main.cpp
+++ main.cpp
@@ -5,19 +5,28 @@
 using namespace std;
 using namespace std::chrono;
 
-enum MEMCPY{
+enum class MEMCPY{
     NORMAL,
     MOVS,
-    STANDARD
+    STANDARD,
 };
-
 extern "C" void memcpy_normal(char* a,char* b, int n);
 extern "C" void memcpy_movs  (char* a,char* b, int n);
 
-const int SIZE = 1000'000'000/4;
-//const int SIZE = 500;
+enum class STRCMP{
+    NORMAL,
+    CMPS,
+    SSE42,
+    STANDARD,
+};
+extern "C" int strcmp_normal(char* a,char* b, int n);
+extern "C" int strcmp_cmps  (char* a,char* b, int n);
+extern "C" int strcmp_sse42 (char* a,char* b, int n);
 
-void test(int id) {
+
+void testMemcpy(MEMCPY id) {
+    const int SIZE = 1000'000'000/4;
+
     char* a=new char[SIZE+1];
     char* b=new char[SIZE+1];
     memset(b, 0, SIZE+1);
@@ -56,9 +65,72 @@ void test(int id) {
     delete [] a;
     delete [] b;
 }
+
+void testStrcmp(STRCMP id) {
+    const int SIZE = 1000'000'000;
+
+    char* a=new char[SIZE+1];
+    char* b=new char[SIZE+1];
+    memset(a, 0, SIZE+1);
+    memset(b, 0, SIZE+1);
+
+    for (int i=0;i<SIZE;++i) {
+        a[i] = 'a' + i%('z'-'a'+1);
+        b[i] = 'a' + i%('z'-'a'+1);
+    }
+
+    b[SIZE/6]+=3;
+
+    high_resolution_clock::time_point t1;
+
+    int result=-1;
+    switch(id) {
+        case STRCMP::NORMAL:
+            cout << "normal: ";
+            t1 = high_resolution_clock::now();
+            result = strcmp_normal(a, b, SIZE);
+            break;
+
+        case STRCMP::CMPS:
+            cout << "cmps  : ";
+            t1 = high_resolution_clock::now();
+            result = strcmp_cmps(a, b, SIZE);
+            break;
+
+        case STRCMP::SSE42:
+            cout << "sse42 : ";
+            t1 = high_resolution_clock::now();
+            result = strcmp_sse42(a, b, SIZE);
+            break;
+
+        case STRCMP::STANDARD:
+            cout << "std   : ";
+            t1 = high_resolution_clock::now();
+            result = strcmp(a, b);
+            break;
+
+        default:
+            assert(0);
+    }
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+    cout << "exec_time=" << duration << " ";
+    cout << "Result=" << result << " ";
+    cout << endl;
+
+    delete [] a;
+    delete [] b;
+}
 int main() {
-    test(MEMCPY::NORMAL);
-    test(MEMCPY::MOVS);
-    test(MEMCPY::STANDARD);
+    cout << "Testing MEMCPY-------------------" << endl;
+    testMemcpy(MEMCPY::NORMAL);
+    testMemcpy(MEMCPY::MOVS);
+    testMemcpy(MEMCPY::STANDARD);
+
+    cout << "Testing STRCMP-------------------" << endl;
+    testStrcmp(STRCMP::NORMAL);
+    testStrcmp(STRCMP::CMPS);
+    testStrcmp(STRCMP::SSE42);
+    testStrcmp(STRCMP::STANDARD);
     return 0;
 }
